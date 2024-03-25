43eb2d2ff0aed0b26e59da9723b345b967c897e4
dacaiguoguo <dacaiguoguo@163.com>
2024-03-25 10:49:59 +0800

    detectFaces

--- VisionTest/ViewController.swift
+++ VisionTest/ViewController.swift
@@ -6,14 +6,192 @@
 //
 
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
+
+func detectFaces(in image: UIImage) {
+    guard let cgImage = image.cgImage else { return }
+
+    let request = VNDetectFaceRectanglesRequest { (request, error) in
+        if let error = error {
+            print("Face detection error: \(error)")
+            return
+        }
+        
+        request.results?.forEach({ result in
+            guard let faceObservation = result as? VNFaceObservation else { return }
+            print("Found a face at \(faceObservation.boundingBox)")
+        })
+    }
+    
+    let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+    do {
+        try handler.perform([request])
+    } catch {
+        print("Failed to perform face detection: \(error)")
+    }
+}
+
+
+func detectFaces(in image: UIImage, completion: @escaping (Int) -> Void) {
+    guard let cgImage = image.cgImage else {
+        completion(0)
+        return
+    }
+
+    let request = VNDetectFaceRectanglesRequest { (request, error) in
+        guard error == nil else {
+            print("Face detection error: \(error!.localizedDescription)")
+            completion(0)
+            return
+        }
+        
+        let faceCount = request.results?.count ?? 0
+        completion(faceCount)
+    }
+    
+    let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+    do {
+        try handler.perform([request])
+    } catch {
+        print("Failed to perform face detection: \(error)")
+        completion(0)
+    }
+}
+
+
+import UIKit
+import Vision
+
+func detectFacesAndShowAlertIfNone(in image: UIImage, viewController: UIViewController, completion: @escaping (Int) -> Void) {
+    DispatchQueue.global(qos: .userInitiated).async {
+        guard let cgImage = image.cgImage else {
+            DispatchQueue.main.async {
+                completion(-1)
+                // showAlert(in: viewController, withMessage: "无法处理图像")
+            }
+            return
+        }
+
+        let request = VNDetectFaceRectanglesRequest { (request, error) in
+            guard error == nil else {
+                DispatchQueue.main.async {
+                    completion(-1)
+                    showAlert(in: viewController, withMessage: "人脸检测出错: \(error!.localizedDescription)")
+                }
+                return
+            }
+            
+            let faceCount = request.results?.count ?? 0
+            DispatchQueue.main.async {
+                completion(faceCount)
+            }
+//            if faceCount == 0 {
+//                DispatchQueue.main.async {
+//                    showAlert(in: viewController, withMessage: "未检测到人脸")
+//                }
+//            }
+        }
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        do {
+            try handler.perform([request])
+        } catch {
+            DispatchQueue.main.async {
+                completion(-1)
+                // showAlert(in: viewController, withMessage: "执行人脸检测失败: \(error)")
+            }
+        }
+    }
+}
+
+func showAlert(in viewController: UIViewController, withMessage message: String) {
+    let alert = UIAlertController(title: "提示", message: message, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "好的", style: .default))
+    viewController.present(alert, animated: true)
+}
+
+class ViewController2: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let tt = UIImage(named: "Business.jpg") {
+            detectFaces(in: tt) { count in
+                print("Found face \(count)")
+            }
+        }
+        if let tt = UIImage(named: "input") {
+            detectFaces(in: tt) { count in
+                print("Found face \(count)")
+            }
+        }
+        if let tt = UIImage(named: "launchIcon3@2x.png") {
+            detectFacesAndShowAlertIfNone(in: tt, viewController: self) { count in
+                if count > 0 {
+                    showAlert(in: self, withMessage: "检测到人脸:\(count)")
+                } else {
+                    showAlert(in: self, withMessage: "没有检测到人脸")
+                }
+            }
+        }
     }
+    
+}
 
+import UIKit
+import PhotosUI
+import Vision
+
+class ViewController: UIViewController, PHPickerViewControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Detect Face"
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "add"), style: .done, target: self, action: #selector(showPhotoPicker))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showPhotoPicker))
+
+    }
+    
+    
+    @objc func showPhotoPicker() {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1 // Limit to one selection
+        configuration.filter = .images // Only show images
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // MARK: - PHPickerViewControllerDelegate
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        guard let itemProvider = results.first?.itemProvider else { return }
+        
+        if itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (image, error) in
+                DispatchQueue.main.async {
+                    if let weakSelf = self {
+                        if let image = image as? UIImage {
+                            detectFacesAndShowAlertIfNone(in: image, viewController: weakSelf) { count in
+                                if count > 0 {
+                                    showAlert(in: weakSelf, withMessage: "检测到人脸:\(count)")
+                                } else {
+                                    showAlert(in: weakSelf, withMessage: "没有检测到人脸")
+                                }
+                            }
+                        } else {
+                            showAlert(in: weakSelf, withMessage: "无法加载图像")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+
+    
 
 }
 
